336a349e5896a835a042d9ffd773f0458e09fdce
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-06-26 14:00:52 +0200

    Add array "movies" and function mapping array elements

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-var element = React.createElement('div', {}, 'Hello world!');
+var movies = [{
+        title: 'Harry Potter',
+        desc: 'film o czarodzieju'
+    },
+    {
+        title: 'Król lew',
+        desc: 'Film o królu sawanny'
+    }
+];
+
+var moviesElements = movies.map(function(movie) {
+    return React.createElement('li', {},
+        React.createElement('h2', {}, movie.title),
+        React.createElement('p', {}, movie.desc)
+    );
+});
+
+var element =
+    React.createElement('div', {},
+        React.createElement('h1', {}, 'Lista filmów'),
+        React.createElement('ul', {}, moviesElements)
+    );
+
 ReactDOM.render(element, document.getElementById('app'));
