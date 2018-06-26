1192c4621aa8d6f3b720a65fff70e30f60e65d09
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-06-26 14:18:53 +0200

    Add new elements in "movies" array containing illustrating images

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 var movies = [{
         id: 1,
         title: 'Harry Potter',
-        desc: 'film o czarodzieju'
+        desc: 'film o czarodzieju',
+        image: './images/harry.jpg'
     },
     {
         id: 2,
         title: 'Król lew',
-        desc: 'Film o królu sawanny'
+        desc: 'Film o królu sawanny',
+        image: './images/lion.jpg'
+    },
+    {
+    	id: 3,
+    	title: 'Władca pierścieni',
+    	desc: 'Film fantasy o zmaganiach dobra ze złem w świecie stworzonym przez J.R.R. Tolkiena',
+    	image: './images/gandalf.jpg'
+    },
+    {
+    	id: 4,
+    	title: 'Przyjaciele',
+    	desc: 'Serial komediowy opisujący życie szóstki przyjaciół żyjących w Nowym Jorku',
+    	image: './images/friends.jpg'
     }
 ];
 
 var moviesElements = movies.map(function(movie) {
     return React.createElement('li', {key: movie.id},
         React.createElement('h2', {}, movie.title),
-        React.createElement('p', {}, movie.desc)
+        React.createElement('p', {}, movie.desc),
+        React.createElement('img', {src: movie.image})
     );
 });
 
